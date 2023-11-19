da8daa08a8dc98ab8411a700a8b68e810c4d4cbe
Artur Sharapov <38008115+ArturSharapov@users.noreply.github.com>
2023-11-19 13:49:10 +0000

    fix: get timestamp without moment

--- src/modules/app/app.controller.ts
+++ src/modules/app/app.controller.ts
@@ -1,5 +1,4 @@
 import { Controller, Get, Query } from '@nestjs/common'
-import moment from 'moment'
 import { RealtimeDatabaseService } from 'src/modules/database/realtime/realtime-database.service'
 import { DEFAULT_POST } from 'src/modules/forum-bot/posts/post.constants'
 import { ForumTasksService } from 'src/modules/forum-bot/tasks/forum-tasks.service'
@@ -16,7 +15,7 @@ export class AppController {
     if (link) {
       try {
         if (!link.startsWith('https://www.chess.com/clubs/forum/view')) throw new TypeError('Invalid url')
-        const timestamp = moment().valueOf()
+        const timestamp = Date.now()
         const postId = await this.realtimeDatabaseService.addNewPost({ ...DEFAULT_POST, dateCreated: timestamp, dateUpdated: timestamp, link })
         this.forumTasksService.checkSpecificPost(postId!)
         return { success: true, message: `Successfully registered new post: ${link}` }
